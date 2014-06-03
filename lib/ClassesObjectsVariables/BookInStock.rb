f8f0c24c616455de15e3222450b2e54605972f0d
stevenGe <steven.infortec@gmail.com>
2014-06-04 07:55:06 +0800

    complete chapter-3

--- lib/ClassesObjectsVariables/BookInStock.rb
+++ lib/ClassesObjectsVariables/BookInStock.rb
@@ -4,8 +4,25 @@
 # @version :  1.0.0-snapshot
 
 class BookInStock
+
+  attr_reader :isbn
+  attr_accessor :price
+
   def initialize(isbn, price)
     @isbn = isbn
-    @price = price
+    @price = Float(price)
   end
+
+  def to_s
+    "ISBN: #{@isbn}, price: #{@price}"
+  end
+
+  def price_in_cents
+    Integer(@price * 100 + 0.5)
+  end
+
+  def price_in_cents=(cents)
+    @price = cents / 100;
+  end
+
 end
--- test/TestBookInStock.rb
+++ test/TestBookInStock.rb
@@ -15,6 +15,19 @@ class TestBookInStock < Test::Unit::TestCase
     # Nothing to do here current now
   end
 
+  def test_initialize
+    b1 = BookInStock.new('isbn1', 3)
+    b2 = BookInStock.new('isbn2', 3.14)
+    b3 = BookInStock.new('isbn3', '5.67')
+    puts b1
+    puts b2
+    puts b3
+    assert_equal(3, b1.price)
+  end
 
+  def test_price_in_cents
+    b2 = BookInStock.new('isbn2', 3.14)
+    assert_equal(314, b2.price_in_cents)
+  end
 
 end
